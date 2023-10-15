fcabbc06b76e7b7ec2ef62f0e56f3f11224bb005
Sveta Selivanova <129683999+SVETASEL@users.noreply.github.com>
2023-10-15 10:49:12 -0300

    New Component

--- src/Components/App/index.tsx
+++ src/Components/App/index.tsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React from 'react';
 import './style.scss';
 
-import { PointsAwardingFields } from "../PointsAwarding/types";
-import PointsAwarding from "../PointsAwarding";
+import { PointsAwardingFields } from '../PointsAwarding/types';
+import PointsAwarding from '../PointsAwarding';
+import ParticipantID from '../ParticipantIDCard';
 
 function App() {
-	const onSubmit = (formFields: PointsAwardingFields) => {
-		console.log(formFields);
-	};
+  const onSubmit = (formFields: PointsAwardingFields) => {
+    console.log(formFields);
+  };
 
-	return (
-		<>
-			<PointsAwarding onSubmit={onSubmit} />
-		</>
-	)
+  return (
+    <>
+      <PointsAwarding onSubmit={onSubmit} />
+      <ParticipantID onSubmit={onSubmit} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/ParticipantIDCard/types.ts
@@ -0,0 +1,5 @@
+export type ParticipantIDFields = {
+  id: string;
+  activity: string;
+  reason: string;
+};
